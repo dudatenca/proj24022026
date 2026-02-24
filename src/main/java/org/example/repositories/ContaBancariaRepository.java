1a4c5683158dd95ae74258c4ab95668e55da7c9c
dudatenca <tencamaria62@gmail.com>
2026-02-24 20:10:10 -0300

    v1

---
+++ src/main/java/org/example/repositories/ContaBancariaRepository.java
@@ -0,0 +1,16 @@
+package org.example.repositories;
+
+import org.example.models.ContaBancaria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContaBancariaRepository {
+
+    public ContaBancaria save (ContaBancaria contaBancaria){
+        return new ContaBancaria();
+    }
+    public List<ContaBancaria> findAll(){
+        return new ArrayList<>();
+    }
+}
